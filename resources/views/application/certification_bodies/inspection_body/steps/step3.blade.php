2fe40776b04b8a26c4ce84999a181c2cd3b90b8d
SheharBano <bshehar2002@gmail.com>
2026-07-01 23:22:52 +0500

    updated

--- resources/views/application/certification_bodies/inspection_body/steps/step3.blade.php
+++ resources/views/application/certification_bodies/inspection_body/steps/step3.blade.php
@@ -4,30 +4,21 @@
 
     {{-- Scope of Inspection --}}
     @include('application.certification_bodies.inspection_body._repeatable_table', [
-        'title'    => 'Scope of Inspection',
+        'title'    => $scopeTitle,
         'target'   => 'ibScopeRows',
         'name'     => 'scopes',
-        'rows'     => $firstRow($scopes, ['description_of_inspection' => '', 'type_and_range' => '', 'methods_and_procedures' => '']),
-        'columns'  => [
-            'description_of_inspection' => 'Description of Inspection',
-            'type_and_range'            => 'Type and Range',
-            'methods_and_procedures'    => 'Methods and Procedures',
-        ],
+        'rows'     => $firstRow($scopes, array_fill_keys(array_keys($scopeCols), '')),
+        'columns'  => $scopeCols,
         'isLocked' => $isLocked,
     ])
 
     {{-- Equipment --}}
     @include('application.certification_bodies.inspection_body._repeatable_table', [
-        'title'    => 'Equipment',
+        'title'    => $equipTitle,
         'target'   => 'ibEquipRows',
         'name'     => 'equipment',
-        'rows'     => $firstRow($equipment, ['equipment_name' => '', 'calibration_organization' => '', 'calibration_frequency' => '', 'last_calibration_date' => '']),
-        'columns'  => [
-            'equipment_name'          => 'Equipment Name',
-            'calibration_organization'=> 'Calibration Organization',
-            'calibration_frequency'   => 'Calibration Frequency',
-            'last_calibration_date'   => 'Last Calibration Date',
-        ],
+        'rows'     => $firstRow($equipment, array_fill_keys(array_keys($equipCols), '')),
+        'columns'  => $equipCols,
         'isLocked' => $isLocked,
     ])
 
